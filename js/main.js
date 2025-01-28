d8d56f9b58e594669317c4ae1959afd917d53284
QuomoX <valeev.ilmar@bk.ru>
2025-01-28 14:56:30 +0400

    массивы_тест_версия

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-//document.write("JavaScript говорит привет!");
-//console.log("JavaScript говорит привет!");
-//console.info("JavaScript говорит привет!");
-//console.error("JavaScript говорит привет!");
-console.warn("JavaScript говорит привет!");
+var arr = [5, true, "stroka", 5.7 , 0 , -100];
+arr[3] = "word";
+console.log(arr[0]);
