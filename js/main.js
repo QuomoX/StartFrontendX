675bc1109b8c473b1944f5ca2dc7e63d4c19d103
QuomoX <valeev.ilmar@bk.ru>
2025-01-31 17:36:09 +0400

    Button

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
-function click() {
-    
-    alert("отлично");
+var counter = 0;
+
+function clickb(el) {
+    counter++;
+    el.innerHTML = "Вы нажали на кнопку: " + counter;
+
+    // el.style.background = "red";
+    // el.style.color = "blue";
+
+    el.style.cssText = "border-radius: 5px; border: 0; fonr-size: 20px";
+
+    console.log(el.onclick);
+
+}
+
+function onInput(el) {
+    if(el.value == "Hello")
+alert ("and you");
+
+    console.log(el.value);
 }
