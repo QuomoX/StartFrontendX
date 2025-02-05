e625fe88e70c011510a8d0fae8768b273c570d69
QuomoX <valeev.ilmar@bk.ru>
2025-02-05 17:35:22 +0400

    13/1

--- js/main.js
+++ js/main.js
@@ -1,37 +1,21 @@
 var counter = 0;
-
 function clickb(el) {
     counter++;
     el.innerHTML = "Вы нажали на кнопку: " + counter;
-
-    // el.style.background = "red";
-    // el.style.color = "blue";
-
     el.style.cssText = "border-radius: 5px; border: 0; fonr-size: 20px";
-
     console.log(el.onclick);
-
 }
-
 function onInput(el) {
     if(el.value == "Hello")
 alert ("and you");
-
     console.log(el.value);
 }
-
 var text = document.getElementById('text');
 text.title = "New text";
 console.log(text.title);
-
 text.style.color = "red";
 text.style.backgroundColor = "blue";
-
-text.innerHTML = "New<br>string";
-
-// document.getElementById('text').style.color = "white";
-
-// var spans = document.getElementsByTagName('span');
+text.innerHTML = "Личный кобинет ООО «ТАРАСОВО»";
 
 var spans = document.getElementsByClassName('simple-text');
 
@@ -39,33 +23,34 @@ for(var i = 0; i < spans.length; i++) {
     console.log(spans[i].innerHTML);
 }
 
-function checkForm(el) {
+document.getElementById('main-form').addEventListener("submit, checkForm");
 
-    var name = el.name.value;
+function checkForm(event) {
+    event.preventDefault();
+    var el = document.getElementById('main-form');
+    var name = el.name.value;  
     var pass = el.pass.value;
     var repass = el.repass.value;
     var state = el.state.value;
-
-    var fail = "";
-
-    if(name == "" || pass == "" || state == "")
+ //обьявление переменных формы
+    var fail = ""; // Переменная при неправильно заполненной формы
+    if(name == "" || pass == "" || state == "") // если одно из полей пустое
     fail = "Заполните все поля";
-    else if(name.length < 2 || name.length > 50)
+    else if(name.length < 2 || name.length > 50)  //ограничение на кол-во знаков
         fail = "Введите корректное имя";
-    else if(pass != repass)
+    else if(pass != repass) 
         fail = "Пароли должны совпадать";
     else if(pass.split("&").length > 1)
         fail = "Некорректный пароль";
-
-    if(fail != "") {
+    if(fail != "")  // Если переменная fail равна "" , то ...
         document.getElementById('error').innerHTML = fail;
-
-        return false;
-    } else {
+    else {
         alert("Все данные корректно заполнены");
-return true;
+        window.location = '2str.html'; // Переход после заполнения формы
     }
-    
-    return false;
-
+}
+setInterval(my_func, 1000);
+function my_func() {
+    counter++;
+    console.log("Counter: " + counter); 
 }
